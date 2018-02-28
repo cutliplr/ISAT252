9f9a66b8efbdf16c355635c8650b27ac9195f889
Lauren <lrc317@gmail.com>
2018-02-28 10:09:27 -0500

    python hello hello and tdd

---
+++ python/tdd/test_arithmetic.py
@@ -0,0 +1,10 @@
+# test arithmetic.py
+# test_arithmetic.py fix
+# write a smoke test
+def test_smoke():
+	assert True == True
+	
+# test the sum(a,b) function
+def test_sum():
+	assert sum(3,4) == 7
+		
