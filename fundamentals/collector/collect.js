bb58a8733238e93c40c3789b078bdab0a3ebab45
Lauren <lrc317@gmail.com>
2018-04-19 23:51:11 -0400

    grocery list using collector function

---
+++ fundamentals/collector/collect.js
@@ -0,0 +1,8 @@
+//collector code, sorting elements of food into a grocery list with string and a number
+var myList = [
+			["Bread", 2], 
+			["Potatoes", 7],
+			["Bananas", 5],
+			["Granola", 2], 
+			["Juice", 3]
+			];
